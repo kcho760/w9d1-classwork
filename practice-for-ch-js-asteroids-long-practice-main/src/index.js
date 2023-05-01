567b17ca60dd109160fb097197f946c543edf1a0
Matt Pierson <matt.pierson789@gmail.com>
2023-05-01 16:30:19 -0400

    finished drawing a circle

--- practice-for-ch-js-asteroids-long-practice-main/src/index.js
+++ practice-for-ch-js-asteroids-long-practice-main/src/index.js
@@ -1,18 +1,24 @@
 import MovingObject from "./moving_object.js"
 
-const canvas1 = document.getElementById("game-canvas");
-canvas1.width = 500;
-canvas1.height = 500;
-const ctx = canvas1.getContext("2d");
-
-const mo = new MovingObject({
-    pos: [30, 30],
-    vel: [10, 10],
-    radius: 5,
-    color: "#00FF00"
-    });
 console.log("Webpack is working....")
 
+document.addEventListener("DOMContentLoaded", () => {
+
+    const canvas1 = document.getElementById("game-canvas");
+    canvas1.width = 500;
+    canvas1.height = 500;
+    const ctx = canvas1.getContext("2d");
+
+    const mo = new MovingObject({
+    pos: [30, 30],
+    vel: [10, 10],
+    radius: 500,
+    color: "#00FF00"
+    });
+
+    mo.draw(ctx);
+
+}) 
 
 window.MovingObject = MovingObject
 //instatntate moving object instance invoke draw  pass in ctx
--- practice-for-ch-js-asteroids-long-practice-main/src/moving_object.js
+++ practice-for-ch-js-asteroids-long-practice-main/src/moving_object.js
@@ -1,24 +1,21 @@
 class MovingObject{
-    constructor(pos,vel, radius, color){
-        this.pos = pos;
-        this.vel = vel;
-        this.radius = radius;
-        this.color = color;
-}
-} ;
-function draw(ctx) {
+    constructor(options){
+        this.pos = options.pos;
+        this.vel = options.vel;
+        this.radius = options.radius;
+        this.color = options.color;
+
+    }
+
+    draw(ctx) {
+    console.log("meow") 
     ctx.beginPath();
     ctx.arc(this.pos[0], this.pos[1], this.radius, 0, 2 * Math.PI);
     ctx.fillStyle = this.color;
     ctx.fill();
-}
+    
+    }
 
-
-
-
-
-
-
-
+} ;
 
 export default MovingObject;
